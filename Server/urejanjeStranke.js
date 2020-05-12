cec479849e92425df06e5ecfb0f584d3173082ae
hanamuzelj <hana.muzelj@student.um.si>
2020-05-12 18:30:54 +0200

    Osnutek urejanja strank

---
+++ Server/urejanjeStranke.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(express.urlencoded({extended:false}));
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host:   '127.0.0.1',
+        user:   'root',
+        password:   'geslo123',
+        database:   'cargo'
+    }
+});
+
+const bookshelf = require('bookshelf')(knex)
