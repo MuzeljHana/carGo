1346993b4a99d7b8beb2e67b56b5eadbd335f7a7
hanamuzelj <hana.muzelj@student.um.si>
2020-05-13 12:53:56 +0200

    Dodajanje bookshelf modelov

--- Server/urejanjeVozil.js
+++ Server/urejanjeVozil.js
@@ -12,3 +12,31 @@ var knex = require('knex')({
         database: 'cargo'
     }
 });
+
+const bookshelf = require('bookshelf')(knex)
+
+var prevozno_podjetje = bookshelf.Model.extend({
+    tableName: 'prevozno_podjetje',
+    idAttribute: 'id'
+});
+
+var prevozno_sredstvo = bookshelf.Model.extend({
+    tableName: 'prevozno_sredstvo',
+    idAttribute: 'id'
+});
+
+var tip_prevoza = bookshelf.Model.extend({
+    tableName: 'tip_prevoza',
+    idAttribute: 'id'
+});
+
+var letnik = bookshelf.Model.extend({
+    tableName: 'letnik',
+    idAttribute: 'id'
+});
+
+var znamka = bookshelf.Model.extend({
+    tableName: 'znamka',
+    idAttribute: 'id'
+});
+
