188c2b50754164336881ed3ed07bbe9d30c37763
hanamuzelj <hana.muzelj@student.um.si>
2020-05-13 12:28:04 +0200

    Urejanje funkcionalnosti

--- Server/urejanjePonudnika.js
+++ Server/urejanjePonudnika.js
@@ -29,3 +29,35 @@ var posta = bookshelf.Model.extend({
     tableName: 'posta',
     idAttribute: 'id'
 });
+
+app.post('/urediPonudnika', async(req, res, next) => {
+    try {
+        let prevozno_podjetjeData = {
+            naziv: req.body.nazivPrevoznega_podjetja,
+            davcna_st: req.body.davcna_stPrevoznega_podjetja,
+            zacetek_dolovanja: req.body.zacetek_dolovanjaPrevoznega_podjetja,
+            uspesnost_poslovanja: req.body.uspesnost_poslovanjaPrevoznega_podjetja,
+            tk_naslov: req.body.idNaslov
+        };
+        let tabelaPrevoznega_podjetja = await new prevozno_podjetje().save(prevozno_podjetjeData)
+        
+        let naslovData = {
+            tk_posta: req.body.idPosta,
+            ulica: req.body.ulica,
+            hisna_st: req.body.hisna_st
+        };
+        let tabelaNaslov = await new naslov().save(naslovData);
+
+        let postaData = {
+            kraj: req.body.kraj,
+            postna_st: req.body.postna_st
+        };
+        let tabelaPosta = await new posta().save(postaData);
+           
+        res.status(200).send("Ponudnik je bil uspešno urejen.");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+app.listen(3000);
