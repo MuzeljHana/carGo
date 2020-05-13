8ac4eb1d2527e7f1002bc2a92c9ab5816c2f4f62
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-13 21:27:24 +0200

    Added a few new models

--- config/models.js
+++ config/models.js
@@ -10,5 +10,27 @@ module.exports = {
     Znamka: bookshelf.model('Znamka', {
         tableName: 'znamka',
         idAttribute: 'id'
-    })
+    }),
+
+    Vozilo: bookshelf.model('Vozilo', {
+        tableName: 'prevozno_sredstvo',
+        idAttribute: 'id'
+    }),
+
+    Podjetje: bookshelf.model('Podjetje', {
+        tableName: 'prevozno_podjetje',
+        idAttribute: 'id'
+    }),
+
+    Tip_prevoza: bookshelf.model('Tip_prevoza', {
+        tableName: 'tip_prevoza',
+        idAttribute: 'id'
+    }),
+
+    Letnik: bookshelf.model('Letnik', {
+        tableName: 'letnik',
+        idAttribute: 'id'
+    }),
+
+    
 }
