bf708c120b404a15d01909c999bf3afc531bc64e
hanamuzelj <hana.muzelj@student.um.si>
2020-05-15 10:50:40 +0200

    Posodobitev funkcionalnosti za urejanje ponudnikov

--- routes/editOwner.js
+++ routes/editOwner.js
@@ -1,3 +1,35 @@
 const table = require('../config/models');
 const express = require('express');
 const router = express.Router();
+
+router.post('/editOwner', async(req, res, next) => {
+    try {
+        let prevozno_podjetjeData = {
+            naziv: req.body.nazivPrevoznega_podjetja,
+            davcna_st: req.body.davcna_stPrevoznega_podjetja,
+            zacetek_delovanja: req.body.zacetek_delovanjaPrevoznega_podjetja,
+            uspesnost_poslovanja: req.body.uspesnost_poslovanjaPrevoznega_podjetja,
+            tk_naslov: req.body.idNaslov
+        };
+        let tabelaPrevoznega_podjetja = await new prevozno_podjetje().save(prevozno_podjetjeData)
+        
+        let naslovData = {
+            tk_posta: req.body.idPosta,
+            ulica: req.body.ulica,
+            hisna_st: req.body.hisna_st
+        };
+        let tabelaNaslov = await new naslov().save(naslovData);
+
+        let postaData = {
+            kraj: req.body.kraj,
+            postna_st: req.body.postna_st
+        };
+        let tabelaPosta = await new posta().save(postaData);
+           
+        res.status(200).send("Ponudnik je bil uspešno urejen.");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
