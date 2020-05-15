1bf619420c81843365797f942aa5b434409d2467
hanamuzelj <hana.muzelj@student.um.si>
2020-05-15 11:37:43 +0200

    Popravki

--- routes/vehicle.js
+++ routes/vehicle.js
@@ -130,4 +130,17 @@ router.post('/editVehicle', async(req, res, next) => {
     }
 });
 
+router.post('/checkActivity', async(req, res, next) => {
+    try {  
+        let prevozno_sredstvoDataAktivnosti= {
+            aktivnost: req.body.aktivnostPrevoznega_sredstva
+        }
+        let tabelaAktivnosti = await new Vozilo().save(prevozno_sredstvoDataAktivnosti)
+
+        res.status(200).send("Activity was successfully checked.");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
