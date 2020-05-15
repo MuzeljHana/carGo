9eb0649161956baf07ec4eb979d76332d73aadc5
hanamuzelj <hana.muzelj@student.um.si>
2020-05-15 10:56:27 +0200

    Odpravljanje napak pri funkcionalnosti

--- routes/checkActivity.js
+++ routes/checkActivity.js
@@ -2,14 +2,14 @@ const table = require('../config/models');
 const express = require('express');
 const router = express.Router();
 
-router.post('/oznaciAktivnost', async(req, res, next) => {
+router.post('/checkActivity', async(req, res, next) => {
     try {  
         let prevozno_sredstvoDataAktivnosti= {
             aktivnost: req.body.aktivnostPrevoznega_sredstva
         }
         let tabelaAktivnosti = await new prevozno_sredstvo().save(prevozno_sredstvoDataAktivnosti)
 
-        res.status(200).send("Aktivnost je bila uspešno označena!");
+        res.status(200).send("Activity was successfully checked.");
     } catch (error) {
         res.status(500).json(error);
     }
