44b461684b43a2ec39eea6d8328b3c3d50456ebd
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2020-05-15 10:14:16 +0200

    Update _routes.js

--- routes/_routes.js
+++ routes/_routes.js
@@ -2,10 +2,12 @@ const example = require('./example');
 const page = require('./page');
 const vehicle = require('./vehicle');
 const order = require('./order');
+const confirmation = require('./confirmation');
 
 module.exports = function (app) {
     app.use('/example', example)
     app.use('/', page);
     app.use('/vehicle', vehicle);
     app.use('/order', order);
+    app.use('/confirmation', confirmation);
 }
