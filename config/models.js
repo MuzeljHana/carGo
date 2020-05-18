7ba4e4663f9f41cc038ca493f2556b478fc47848
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-18 19:25:13 +0200

    Changed models to new database

--- config/models.js
+++ config/models.js
@@ -2,79 +2,76 @@ const knex = require('./database');
 const bookshelf = require('bookshelf')(knex);
 
 module.exports = {
-    Termin: bookshelf.model('Termin', {
-        tableName: 'termin',
-        idAttribute: 'id'
-    }),
-
-    Znamka: bookshelf.model('Znamka', {
-        tableName: 'znamka',
-        idAttribute: 'id'
-    }),
-
-    Vozilo: bookshelf.model('Vozilo', {
-        tableName: 'prevozno_sredstvo',
-        idAttribute: 'id'
-    }),
-
-    Podjetje: bookshelf.model('Podjetje', {
-        tableName: 'prevozno_podjetje',
-        idAttribute: 'id'
-    }),
-
-    Tip_prevoza: bookshelf.model('Tip_prevoza', {
-        tableName: 'tip_prevoza',
-        idAttribute: 'id'
-    }),
-
-    Letnik: bookshelf.model('Letnik', {
-        tableName: 'letnik',
-        idAttribute: 'id'
-    }),
-
-    Uporabnik: bookshelf.model('Uporabnik', {
-        tableName: 'iskalec_prevoza',
-        idAttribute: 'id'
-    }),
-
-    Tip_uporabnika: bookshelf.model('Tip_uporabnika', {
-        tableName: 'tip_iskalca',
-        idAttribute: 'id'
-    }),
-
-    Tovor: bookshelf.model('Tovor', {
-        tableName: 'tovor',
-        idAttribute: 'id'
-    }),
-
-    Destinacija: bookshelf.model('Destinacija', {
-        tableName: 'destinacija',
-        idAttribute: 'id'
+    Posta: bookshelf.model('Posta', {
+        tableName: 'Posta',
+        naslov() {
+            return this.hasMany('Naslov');
+        }
     }),
 
     Naslov: bookshelf.model('Naslov', {
-        tableName: 'naslov',
-        idAttribute: 'id'
+        tableName: 'Naslov',
+        uporabnik() {
+            return this.hasMany('Uporabnik');
+        },
+        ponudba() {
+            return this.hasMany('Ponudba');
+        }
     }),
 
-    Posta: bookshelf.model('Posta', {
-        tableName: 'posta',
-        idAttribute: 'id'
+    Uporabnik: bookshelf.model('Uporabnik', {
+        tableName: 'Uporabnik',
+        vozilo() {
+            return this.hasMany('Vozilo');
+        },
+        ponudba() {
+            return this.hasMany('Ponudba');
+        }
+    }),
+
+    Tip_vozila: bookshelf.model('Tip_vozila', {
+        tableName: 'Tip_vozila',
+        vozilo() {
+            return this.hasMany('Vozilo');
+        }
+    }),
+
+    Znamka: bookshelf.model('Znamka', {
+        tableName: 'Znamka',
+        vozilo() {
+            return this.hasMany('Vozilo');
+        }
+    }),
+
+    Vozilo: bookshelf.model('Vozilo', {
+        tableName: 'Vozilo',
+        ponudba() {
+            return this.hasMany('Ponudba');
+        },
+        cenik() {
+            return this.hasMany('Cenik');
+        }
     }),
 
     Cenik: bookshelf.model('Cenik', {
-        tableName: 'cenik',
-        idAttribute: 'id'
+        tableName: 'Cenik'
+    }),
+
+    Ponudba: bookshelf.model('Ponudba', {
+        tableName: 'Ponudba',
+        izdelek() {
+            return this.hasMany('Izdelek'); 
+        }
+    }),
+
+    Izdelek: bookshelf.model('Izdelek', {
+        tableName: 'Izdelek'
     }),
 
     Tip_tovora: bookshelf.model('Tip_tovora', {
-        tableName: 'tip_tovora',
-        idAttribute: 'id'
-    }),
-    
-    Povezava: bookshelf.model('Povezava', {
-        tableName: 'tovor_has_iskalec_prevoza',
-        idAttribute: 'id'
+        tableName: 'Tip_tovora',
+        izdelek() {
+            return this.hasMany('Ponudba'); 
+        }
     })
-
 }
