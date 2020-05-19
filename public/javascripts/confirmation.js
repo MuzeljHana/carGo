726523719d64435329f25547141b5a47d87ad810
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2020-05-19 20:10:19 +0200

    Pridobivanje podatkov s strani

--- public/javascripts/confirmation.js
+++ public/javascripts/confirmation.js
@@ -1,3 +1,11 @@
 function confirm() {
-
+    //  pridobivanje podatkov s spletne strani
+    let casNalozitve = document.getElementById("casNalozitve").innerHTML;
+    let status = document.getElementById("status").innerHTML;
+    let casPonudbe = document.getElementById("casPonudbe").innerHTML;
+    let pripombe = document.getElementById("pripombe").value;
+    let tezaTovora = document.getElementById("tezaTovora").value;
+    let volumenTovora = document.getElementById("volumen").value;
+    let stPalet = document.getElementById("stPalet").value;
+    let tezaPalet = document.getElementById("tezaPalet").value;
 }
