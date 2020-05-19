cfbc9bdab737e9497e24fc50cbde234c77f7f13c
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-19 13:24:07 +0200

    Changed to new database and sessions and removed unnecessary routes

--- routes/vehicle.js
+++ routes/vehicle.js
@@ -116,12 +116,20 @@ router.post('/', (req, res, next) => {
 });
 
 router.put('/:id/active/:bool', (req, res, next) => {
-    new table.Vozilo({ id: req.params.id }).save({ aktivnost: req.params.bool })
-        .then(() => {
-            res.json({ "message": "success" });
+    knex('Vozilo')
+        .update({
+            aktivno: req.params.bool
+        })
+        .where({
+            idUporabnik: req.session.user_id,
+            id: req.params.id
+        })
+        .then((data) => {
+            res.json(data);
         })
         .catch((err) => {
-            res.status(500).json({ "message": err });
+            console.log(err);
+            res.status(500).send();
         });
 });
 
@@ -136,51 +144,19 @@ router.put('/:id/occupied/:bool', (req, res, next) => {
 });
 
 router.delete('/:id', (req, res, next) => {
-    new table.Vozilo({ id: req.params.id }).destroy()
-        .then(() => {
-            res.json({ "message": "success" });
+    knex('Vozilo')
+        .del()
+        .where({
+            idUporabnik: req.session.user_id,
+            id: req.params.id
+        })
+        .then((data) => {
+            res.json(data);
         })
         .catch((err) => {
-            res.status(500).json({ "message": err });
+            console.log(err);
+            res.status(500).send();
         });
-});
-
-router.post('/editVehicle', async (req, res, next) => {
-    let podjetje, tip, znamka, letnik;
-    try {
-        podjetje = await new table.Podjetje({ naziv: req.body.podjetje }).fetch({ columns: ['id'] });
-        tip = await new table.Tip_prevoza({ naziv: req.body.tip }).fetch({ columns: ['id'] });
-        znamka = await new table.Znamka({ naziv: req.body.znamka }).fetch({ columns: ['id'] });
-        letnik = await new table.Letnik({ naziv: req.body.letnik }).fetch({ columns: ['id'] });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ "message": err });
-    }
-
-    let data = {
-        id: req.body.vozilo,
-        registracijska_st: req.body.registracijska_st,
-        max_teza: req.body.max_teza_tovora,
-        cena: req.body.cena_km,
-        potrdilo_izpravnosti: req.body.potrdilo_izpravnosti,
-        aktivnost: req.body.aktivnost,
-        zasedenost: req.body.zasedenost,
-        volumen: req.body.volumen,
-        dolzina: req.body.dolzina,
-        sirina: req.body.sirina,
-        visina: req.body.visina,
-        st_palet: req.body.st_pelet,
-        tk_znamka: znamka.get('id'),
-        tk_tip_prevoza: tip.get('id'),
-        tk_letnik: letnik.get('id'),
-        tk_prevozno_podjetje: podjetje.get('id')
-    }
-    new table.Vozilo().save(data).then(() => {
-        res.json({ "message": "success" });
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json({ "message": err });
-    });
 });
 
 module.exports = router;
