b38c83e62d5eb513369e5eb67c68091f169258c6
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-19 13:23:45 +0200

    Changed to new database and sessions and removed unnecessary routes

--- routes/vehicle.js
+++ routes/vehicle.js
@@ -83,42 +83,36 @@ router.get('/:id', (req, res, next) => {
         });
 });
 
-router.post('/add', async (req, res, next) => {
-    let podjetje, tip, znamka, letnik;
-    try {
-        podjetje = await new table.Podjetje({ naziv: req.body.podjetje }).fetch({ columns: ['id'] });
-        tip = await new table.Tip_prevoza({ naziv: req.body.tip }).fetch({ columns: ['id'] });
-        znamka = await new table.Znamka({ naziv: req.body.znamka }).fetch({ columns: ['id'] });
-        letnik = await new table.Letnik({ naziv: req.body.letnik }).fetch({ columns: ['id'] });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ "message": err });
-    }
+router.post('/', (req, res, next) => {
+    knex.into('Vozilo')
+        .insert([{
+            letnik: req.body.letnik,
+            registerska: req.body.registerska,
+            model: req.body.model,
+            maks_teza_tovora: req.body.maks_teza_tovora,
+            potrdilo_izpravnosti: req.body.potrdilo_izpravnosti,
+            maks_volumen_tovora: req.body.maks_volumen_tovora,
+            maks_dolzina_tovora: req.body.maks_dolzina_tovora,
+            maks_dolzina_tovora: req.body.maks_dolzina_tovora,
+            maks_sirina_tovora: req.body.maks_sirina_tovora,
+            maks_visina_tovora: req.body.maks_visina_tovora,
+            maks_st_palet: req.body.maks_st_palet,
 
-    let data = {
-        registracijska_st: req.body.registracijska_st,
-        max_teza: req.body.max_teza_tovora,
-        cena: req.body.cena_km,
-        potrdilo_izpravnosti: req.body.potrdilo_izpravnosti,
-        aktivnost: req.body.aktivnost,
-        zasedenost: req.body.zasedenost,
-        volumen: req.body.volumen,
-        dolzina: req.body.dolzina,
-        sirina: req.body.sirina,
-        visina: req.body.visina,
-        st_palet: req.body.st_pelet,
-        tk_prevozno_podjetje: podjetje.get('id'),
-        tk_znamka: znamka.get('id'),
-        tk_tip_prevoza: tip.get('id'),
-        tk_letnik: letnik.get('id')
-    }
-    new table.Vozilo().save(data).then(() => {
-        res.json({ "message": "success" });
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json({ "message": err });
-    });
-
+            idUporabnik: req.session.user_id,
+            idZnamka: (qb) => {
+                qb.from("Znamka").select("id").where({ naziv: req.body.znamka });
+            },
+            idTip_vozila: (qb) => {
+                qb.from("Tip_vozila").select("id").where({ naziv: req.body.tip_vozila });
+            }
+        }])
+        .then((data) => {
+            res.send();
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send();
+        });
 });
 
 router.put('/:id/active/:bool', (req, res, next) => {
