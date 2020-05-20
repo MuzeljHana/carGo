cb07190b9b2bdb3c86802ed06cb04d02fd2b9a00
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2020-05-20 09:50:31 +0200

    Popravki

--- public/javascripts/confirmation.js
+++ public/javascripts/confirmation.js
@@ -4,27 +4,41 @@ function confirm() {
     let status = document.getElementById("status").innerHTML;
     let casPonudbe = document.getElementById("casPonudbe").innerHTML;
     let pripombe = document.getElementById("pripombe").value;
-    let tezaTovora = document.getElementById("tezaTovora").value;
-    let volumenTovora = document.getElementById("volumen").value;
-    let stPalet = document.getElementById("stPalet").value;
+    let teza = document.getElementById("tezaTovora").value;
+    let volumen = document.getElementById("volumen").value;
+    let steviloPalet = document.getElementById("stPalet").value;
     let tezaPalet = document.getElementById("tezaPalet").value;
 
     //  tuji ključ
     let idUporabnik = sessionStorage.getItem("idUporabnik");
-    let idVozilo = document.getElementById("vehicleID").innerHTML;
+    let vozilo = document.getElementById("vehicleID").innerHTML;
 
     //  informacije za pridobivanje tujih ključev
     let tipTovora = document.getElementById("tipTovora").value;
     let nalozitev = document.getElementById("nalozitev").innerHTML;
     let dostava = document.getElementById("dostava").innerHTML;  
     
-    fetch('http://localhost:3000/confirmation/', {
+    let podatki = {
+        'casNalozitve': casNalozitve,
+        'status': status,
+        'casPonudbe': casPonudbe,
+        'pripombe': pripombe,
+        'teza': teza,
+        'volumen': volumen,
+        'steviloPalet': steviloPalet,
+        'tezaPalet': tezaPalet,
+        'idUporabnik': idUporabnik,
+        'vozilo': vozilo,
+        'tipTovora': tipTovora,
+        'nalozitev': nalozitev,
+        'dostava': dostava
+    }
+
+    fetch('http://localhost:3000/confirmation', {
         method: 'POST',
-        body: JSON.stringify([casNalozitve, status, casPonudbe, pripombe, tezaTovora, volumenTovora, 
-            stPalet, tezaPalet, tipTovora, nalozitev, dostava, idUporabnik,
-            idVozilo, tipTovora, nalozitev, dostava]),
+        body: JSON.stringify(podatki),
         headers: {
-            'Content type': 'application/json'
+            'content-type': 'application/json'
         }
     }).then((response) => {
         console.log("Order successfully confirmed");
