fb0c09451965e694af4075ec5bd907049e9c6a41
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2020-05-20 09:54:06 +0200

    Popravki

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -75,7 +75,7 @@ router.post('/', async(req, res, next) => {
             naslov_nalozitve_idNaslov: nalozitevID,
             naslov_dostave_idNaslov: dostavaID
         }
-        await new table.Ponudba().save(podatki);
+        new table.Ponudba().save(podatki);
         res.status(200).send("Order successfully confirmed.");
     } catch (error) {
         res.status(500).json(error);
