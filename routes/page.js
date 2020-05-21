ba7a8fb4d1094cf894ebff5470d2338f4b3ce149
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-21 18:32:34 +0200

    Added support for different header based on login status

--- routes/page.js
+++ routes/page.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+    req.template_data = { login: req.session.user_id != undefined };
+    next();
+});
+
 router.get('/', (req, res, next) => {
-    res.render('index');
+    res.render('index', req.template_data);
 });
 
 router.get('/search', (req, res, next) => {
-    res.render('search');
+    res.render('search', req.template_data);
 });
 
 router.get('/transports', (req, res, next) => {
-    res.render('transports');
+    res.render('transports', req.template_data);
 });
 
 router.get('/dashboard/vehicle', (req, res, next) => {
-    res.render('vehicle');
+    res.render('vehicle', req.template_data);
 });
 
 router.get('/login', (req, res, next) => {
-    res.render('login');
+    res.render('login', req.template_data);
 });
 
 router.get('/register', (req, res, next) => {
-    res.render('register');
+    res.render('register', req.template_data);
 });
 
 module.exports = router;
