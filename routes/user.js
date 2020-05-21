00be5f05cbe6c2f4ac8a7439fa1b7449c08f803d
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-21 18:30:20 +0200

    Restructure /register and added password check

--- routes/user.js
+++ routes/user.js
@@ -4,40 +4,45 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/register', async (req, res, next) => {
-    if (!PostaExists(req.body.kraj, req.body.postna_stevilka)) {
-        await knex.into("Posta").insert([{ kraj: req.body.kraj, stevilka: req.body.postna_stevilka }]);
-    }
-    if (!NaslovExists(req.body.ulica, req.body.hisna_stevilka)) {
-        await knex.into("Naslov").insert([{ 
-                ulica: req.body.ulica, 
-                stevilka: req.body.hisna_stevilka, 
-                idPosta:  (qb) => {
+    if (! await UserExists(req.body.email, req.body.davcna)) {
+        const regex = /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\S{6,20})$/; //Uppercase, lowercase, number, 6 - 20 characters
+        if (!regex.test(req.body.geslo.trim())) {
+            res.json({ message: "password format incorrect" });
+            return;
+        }
+        if (! await PostaExists(req.body.kraj, req.body.postna_stevilka)) {
+            await knex.into("Posta").insert([{ kraj: req.body.kraj, stevilka: req.body.postna_stevilka }]);
+        }
+        if (! await NaslovExists(req.body.ulica, req.body.hisna_stevilka)) {
+            await knex.into("Naslov").insert([{
+                ulica: req.body.ulica,
+                stevilka: req.body.hisna_stevilka,
+                idPosta: (qb) => {
                     qb.from("Posta")
                         .select("id")
                         .where({ kraj: req.body.kraj })
                         .orWhere({ stevilka: req.body.postna_stevilka });
                 }
             }]);
-    }
-    let data = {
-        ime: req.body.ime,
-        priimek: req.body.priimek,
-        email: req.body.email,
-        geslo: bcrypt.hashSync(req.body.geslo, 10),
-        idNaslov: (qb) => {
-            qb.from("Naslov").select("id").where({ ulica: req.body.ulica, stevilka: req.body.hisna_stevilka });
         }
-    };
-    if (req.body.naziv_podjetja) {
-        let podjetje_data = {
-            naziv_podjetja: req.body.naziv_podjetja,
-            davcna: req.body.davcna,
-            zacetek_delovanja: req.body.zacetek_delovanja,
-            uspesnost_poslovanja: req.body.uspesnost_poslovanja
+        let data = {
+            ime: req.body.ime,
+            priimek: req.body.priimek,
+            email: req.body.email,
+            geslo: bcrypt.hashSync(req.body.geslo.trim(), 10),
+            idNaslov: (qb) => {
+                qb.from("Naslov").select("id").where({ ulica: req.body.ulica, stevilka: req.body.hisna_stevilka });
+            }
+        };
+        if (req.body.naziv_podjetja) {
+            let podjetje_data = {
+                naziv_podjetja: req.body.naziv_podjetja,
+                davcna: req.body.davcna,
+                zacetek_delovanja: req.body.zacetek_delovanja,
+                uspesnost_poslovanja: req.body.uspesnost_poslovanja
+            }
+            data = Object.assign(data, podjetje_data);
         }
-        data = Object.assign(data, podjetje_data);
-    }
-    if (!UserExists(req.body.email, req.body.davcna)) {
         knex.into("Uporabnik")
             .insert([data])
             .then((data) => {
@@ -48,7 +53,7 @@ router.post('/register', async (req, res, next) => {
                 res.status(500).send();
             });
     } else {
-        res.json({ "message": "User exists" });
+        res.json({ message: "User exists" });
     }
 });
 
@@ -57,11 +62,11 @@ router.post("/login", (req, res, next) => {
         .select("id", "geslo")
         .where({ email: req.body.email })
         .then((user) => {
-            if (user.length != 0 && bcrypt.compareSync(req.body.geslo, user[0].geslo)) {
+            if (user.length != 0 && bcrypt.compareSync(req.body.geslo.trim(), user[0].geslo)) {
                 req.session.user_id = user[0].id;
                 res.send();
             } else {
-                res.json({ "message": "login failed" });
+                res.json({ message: "login failed" });
             }
         })
         .catch((err) => {
@@ -74,24 +79,24 @@ module.exports = router;
 
 async function PostaExists(kraj, stevilka) {
     let posta = await knex.from("Posta as p")
-                        .select("p.id")
-                        .where({ kraj: kraj })
-                        .orWhere({ stevilka: stevilka });
+        .select("p.id")
+        .where({ kraj: kraj })
+        .orWhere({ stevilka: stevilka });
     return posta.length != 0;
 }
 
 async function NaslovExists(ulica, stevilka) {
     let naslov = await knex.from("Naslov as n")
-                        .select("n.id")
-                        .where({ ulica: ulica, "n.stevilka": stevilka })
-                        .join("Posta as p", { 'n.idPosta': 'p.id' });
+        .select("n.id")
+        .where({ ulica: ulica, "n.stevilka": stevilka })
+        .join("Posta as p", { 'n.idPosta': 'p.id' });
     return naslov.length != 0;
 }
 
 async function UserExists(email, davcna) {
     let user = await knex.from("Uporabnik")
-                        .select("id")
-                        .where({ email: email })
-                        .orWhere({ davcna: davcna || 0 });
+        .select("id")
+        .where({ email: email })
+        .orWhere({ davcna: davcna || 0 });
     return user.length != 0;
 }
