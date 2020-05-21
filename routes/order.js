98164532b86dcc35e6c3cc42e7fc609307e475af
Viktorija Klinc <viktorija.klinc@student.um.si>
2020-05-21 13:53:22 +0200

    posodobitev na novo podatkovno bazo

--- routes/order.js
+++ routes/order.js
@@ -1,14 +1,39 @@
+const auth = require('./auth');
 const table = require('../config/models');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    new table.Ponudba().fetchAll()
-        .then((ponudbe) => {
-            res.json(ponudbe);
+router.use(auth);
+
+router.get('/:id', (req, res, next) => {
+    knex.from('Ponudba as p')
+        .select([
+            "p.id",
+            "cas_nalozitve",
+            "status",
+            "cas_ponudbe",
+            "pripombe",
+            "teza_tovora",
+            "volumen_tovora",
+            "st_palet",
+            "teza_palet",
+            "idVozilo",
+            "idTip_tovora",
+            "naslov_nalozitve_idNaslov",
+            "naslov_dostave_idNaslov"])
+        .where({
+            "idUporabnik": req.session.user_id,
+        })
+        .join("Vozilo as v", { 'v.id': 'p.idVozilo' })
+        .join("Tip_tovora as t", { 't.id': 'p.idTip_tovora' })
+        .join("Naslov as n", { 'n.id': 'p.naslov_nalozitve_idNaslov' })
+        .join("Naslov as n", { 'n.id': 'p.naslov_dostave_idNaslov' })
+        .then((data) => {
+            res.json(data);
         })
         .catch((err) => {
-            res.status(500).json(err);
+            console.log(err);
+            res.status(500).send();
         });
 });
 
