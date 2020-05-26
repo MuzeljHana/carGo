7458e6938575a7fc2f72033acbd126b320c17611
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-26 20:56:40 +0200

    Added order functionality

--- routes/order.js
+++ routes/order.js
@@ -15,18 +15,89 @@ router.get('/', auth, (req, res, next) => {
             "volumen_tovora",
             "st_palet",
             "teza_palet",
-            "idVozilo",
-            "idTip_tovora",
-            "naslov_nalozitve_idNaslov",
-            "naslov_dostave_idNaslov"])
-        .where({
-            "p.idUporabnik": req.session.user_id,
+            "idVozilo as vozilo",
+            "t.naziv as tip_tovora",
+            "naslov_nalozitve_idNaslov as nalozisce",
+            "naslov_dostave_idNaslov as dostava",
+        ])
+        .whereNotIn("status", ["zavrnjeno", "koncano"])
+        .andWhere((qb) => {
+            qb.where({ "p.idUporabnik": req.session.user_id })
+                .orWhere({ "v.idUporabnik": req.session.user_id })
         })
         .join("Vozilo as v", { 'v.id': 'p.idVozilo' })
         .join("Tip_tovora as t", { 't.id': 'p.idTip_tovora' })
-        .join("Naslov as n1", { 'n1.id': 'p.naslov_nalozitve_idNaslov' })
-        .join("Naslov as n2", { 'n2.id': 'p.naslov_dostave_idNaslov' })
-        .then((data) => {
+        .join("Naslov as nalozitev", { 'nalozitev.id': 'p.naslov_nalozitve_idNaslov' })
+        .join("Naslov as dostava", { 'dostava.id': 'p.naslov_dostave_idNaslov' })
+        .then(async (data) => {
+            if (data) {
+                for (let order of data) {
+                    vozilo = await knex.from('Vozilo as v')
+                        .select([
+                            "letnik",
+                            "model",
+                            "maks_teza_tovora",
+                            "maks_volumen_tovora",
+                            "maks_dolzina_tovora",
+                            "maks_sirina_tovora",
+                            "maks_visina_tovora",
+                            "maks_st_palet",
+                            "t.naziv as tip_vozila",
+                            "z.naziv as znamka",
+                            "c.cena_na_km"
+                        ])
+                        .where({
+                            "idUporabnik": req.session.user_id,
+                            "v.id": order.vozilo,
+                            "c.datum_od": (qb) => {
+                                qb.from("Cenik as c").max("datum_od")
+                                    .whereRaw("datum_od <= p.cas_ponudbe", [])
+                                    .andWhere({ "p.id": order.id })
+                                    .join("Vozilo as v", { 'c.idVozilo': 'v.id' })
+                                    .join("Ponudba as p", { 'p.idVozilo': 'v.id' });
+                            }
+                        })
+                        .join("Tip_vozila as t", { 't.id': 'v.idTip_vozila' })
+                        .join("Znamka as z", { 'z.id': 'v.idZnamka' })
+                        .join("Cenik as c", { 'c.idVozilo': 'v.id' })
+                    order.vozilo = vozilo[0];
+
+                    let nalozisce = await knex.from('Naslov as n')
+                        .select([
+                            "ulica",
+                            "n.stevilka",
+                            "p.kraj",
+                            "p.stevilka as posta"
+                        ])
+                        .where({ "n.id": order.nalozisce })
+                        .join("Posta as p", { 'p.id': 'n.idPosta' })
+                    order.nalozisce = nalozisce[0];
+
+                    let dostava = await knex.from('Naslov as n')
+                        .select([
+                            "ulica",
+                            "n.stevilka",
+                            "p.kraj",
+                            "p.stevilka as posta"
+                        ])
+                        .where({ "n.id": order.dostava })
+                        .join("Posta as p", { 'p.id': 'n.idPosta' })
+                    order.dostava = dostava[0];
+
+                    if (order.tip_tovora == "posamezni izdelki") {
+                        let izdelki = await knex.from('Izdelek as i')
+                            .select([
+                                "teza",
+                                "dolzina",
+                                "visina",
+                                "sirina",
+                                "kolicina"
+                            ])
+                            .join("Ponudba as p", { 'p.id': 'i.idPonudba' })
+                        order.izdelki = izdelki;
+                    }
+                }
+            }
             res.json(data);
         })
         .catch((err) => {
@@ -38,7 +109,6 @@ router.get('/', auth, (req, res, next) => {
 router.get('/:id', auth, (req, res, next) => {
     knex.from('Ponudba as p')
         .select([
-            "p.id",
             "cas_nalozitve",
             "status",
             "cas_ponudbe",
@@ -47,19 +117,90 @@ router.get('/:id', auth, (req, res, next) => {
             "volumen_tovora",
             "st_palet",
             "teza_palet",
-            "idVozilo",
-            "idTip_tovora",
-            "naslov_nalozitve_idNaslov",
-            "naslov_dostave_idNaslov"])
-        .where({
-            "p.idUporabnik": req.session.user_id,
-            "p.id": req.params.id
+            "idVozilo as vozilo",
+            "t.naziv as tip_tovora",
+            "naslov_nalozitve_idNaslov as nalozisce",
+            "naslov_dostave_idNaslov as dostava"
+        ])
+        .whereNotIn("status", ["zavrnjeno", "koncano"])
+        .andWhere({ "p.id": req.params.id })
+        .andWhere((qb) => {
+            qb.where({ "p.idUporabnik": req.session.user_id })
+                .orWhere({ "v.idUporabnik": req.session.user_id })
         })
         .join("Vozilo as v", { 'v.id': 'p.idVozilo' })
         .join("Tip_tovora as t", { 't.id': 'p.idTip_tovora' })
-        .join("Naslov as n1", { 'n1.id': 'p.naslov_nalozitve_idNaslov' })
-        .join("Naslov as n2", { 'n2.id': 'p.naslov_dostave_idNaslov' })
-        .then((data) => {
+        .join("Naslov as nalozitev", { 'nalozitev.id': 'p.naslov_nalozitve_idNaslov' })
+        .join("Naslov as dostava", { 'dostava.id': 'p.naslov_dostave_idNaslov' })
+        .then(async (data) => {
+            if (data.length != 0) {
+                let order = data[0];
+                let vozilo = await knex.from('Vozilo as v')
+                    .select([
+                        "letnik",
+                        "registerska",
+                        "model",
+                        "maks_teza_tovora",
+                        "maks_volumen_tovora",
+                        "maks_dolzina_tovora",
+                        "maks_sirina_tovora",
+                        "maks_visina_tovora",
+                        "maks_st_palet",
+                        "t.naziv as tip_vozila",
+                        "z.naziv as znamka",
+                        "c.cena_na_km"
+                    ])
+                    .where({
+                        "idUporabnik": req.session.user_id,
+                        "v.id": order.vozilo,
+                        "c.datum_od": (qb) => {
+                            qb.from("Cenik as c").max("datum_od")
+                                .whereRaw("datum_od <= p.cas_ponudbe", [])
+                                .andWhere({ "p.id": req.params.id })
+                                .join("Vozilo as v", { 'c.idVozilo': 'v.id' })
+                                .join("Ponudba as p", { 'p.idVozilo': 'v.id' });
+                        }
+                    })
+                    .join("Tip_vozila as t", { 't.id': 'v.idTip_vozila' })
+                    .join("Znamka as z", { 'z.id': 'v.idZnamka' })
+                    .join("Cenik as c", { 'c.idVozilo': 'v.id' })
+                order.vozilo = vozilo[0];
+
+                let nalozisce = await knex.from('Naslov as n')
+                    .select([
+                        "ulica",
+                        "n.stevilka",
+                        "p.kraj",
+                        "p.stevilka as posta"
+                    ])
+                    .where({ "n.id": order.nalozisce })
+                    .join("Posta as p", { 'p.id': 'n.idPosta' })
+                order.nalozisce = nalozisce[0];
+
+                let dostava = await knex.from('Naslov as n')
+                    .select([
+                        "ulica",
+                        "n.stevilka",
+                        "p.kraj",
+                        "p.stevilka as posta"
+                    ])
+                    .where({ "n.id": order.dostava })
+                    .join("Posta as p", { 'p.id': 'n.idPosta' })
+                order.dostava = dostava[0];
+
+                if (order.tip_tovora == "posamezni izdelki") {
+                    let izdelki = await knex.from('Izdelek as i')
+                        .select([
+                            "teza",
+                            "dolzina",
+                            "visina",
+                            "sirina",
+                            "kolicina"
+                        ])
+                        .join("Ponudba as p", { 'p.id': 'i.idPonudba' })
+                    order.izdelki = izdelki;
+                }
+            }
             res.json(data);
         })
         .catch((err) => {
@@ -155,6 +296,57 @@ router.post('/', auth, async (req, res, next) => {
             console.log(err);
             res.status(500).send();
         });
+});
+
+router.put('/:id/status/:status', auth, (req, res, next) => {
+    knex('Ponudba as p')
+        .update({
+            status: req.params.status
+        })
+        .where({
+            "p.id": (qb) => {
+                qb.from('Vozilo as v')
+                    .select('v.id')
+                    .join("Ponudba as p", "p.idVozilo", "=", "v.id")
+                    .where({ "p.id": req.params.id, "v.idUporabnik": req.session.user_id });
+            }
+        })
+        .then(() => { })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send();
+        });
+
+    let data;
+    switch (req.params.status) {
+        case "potrjeno":
+            data = { zasedeno: 1 };
+            break;
+        case "koncano":
+            data = { zasedeno: 0 };
+            break;
+    }
+    if (data) {
+        knex('Vozilo as v')
+            .update(data)
+            .where({
+                'v.id': (qb) => {
+                    qb.from('Vozilo as v')
+                        .select('v.id')
+                        .join("Ponudba as p", "p.idVozilo", "=", "v.id")
+                        .where({ "p.id": req.params.id, "v.idUporabnik": req.session.user_id });
+                }
+            })
+            .then(() => {
+                res.json({ message: "success" });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).send();
+            });
+    } else {
+        res.json({ message: "success" });
+    }
 });
 
 module.exports = router;
