2d80b2a5956c99c484c74050296aeec1954d5e2c
Tjan Ljubešek <tjan.ljubesek@student.um.si>
2020-05-26 13:38:55 +0200

    Added vehicle limitations

--- public/javascripts/details.js
+++ public/javascripts/details.js
@@ -6,13 +6,32 @@ $.ajax({
 })
     .done(function (data) {
         if (data) {
-            let vehicle_id = localStorage.getItem("details_vehicle");           
+            let vehicle_id = localStorage.getItem("details_vehicle");
             let vehicle = data.find((veh) => { return veh.id == Number(vehicle_id); });
- 
+
             $("#podjetje").text(vehicle.naziv_podjetja);
             $("#vozilo").text(vehicle.znamka + " " + vehicle.model);
             $("#letnik").text(vehicle.letnik);
             $("#tip").text(vehicle.tip_vozila);
+            $("#cena_na_km").text(vehicle.cena_na_km);
 
+            let omejitve_vozila = $("#omejitve");
+            omejitve_vozila.append("<li>Teža tovora: " + vehicle.maks_teza_tovora + " kg</li>");
+
+            if (vehicle.maks_volumen_tovora) {
+                omejitve_vozila.append("<li>Volumen tovora: " + vehicle.maks_volumen_tovora + " m<sup>3</sup></li>");
+            }
+            if (vehicle.maks_dolzina_tovora) {
+                omejitve_vozila.append("<li>Dolžina tovora: " + vehicle.maks_dolzina_tovora + " cm</li>");
+            }
+            if (vehicle.maks_sirina_tovora) {
+                omejitve_vozila.append("<li>Širina tovora: " + vehicle.maks_sirina_tovora + " cm</li>");
+            }
+            if (vehicle.maks_visina_tovora) {
+                omejitve_vozila.append("<li>Višina tovora: " + vehicle.maks_visina_tovora + " cm</li>");
+            }
+            if (vehicle.maks_st_palet) {
+                omejitve_vozila.append("<li>Število palet: " + vehicle.maks_st_palet + "</li>");
+            }
         }
     });
