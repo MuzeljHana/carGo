7af575e728d0e6adcc952e1104dcef2702435831
Tadej Lahovnik <tadej.lahovnik@student.um.si>
2020-05-28 13:05:55 +0200

    Popravki

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,12 +1,12 @@
 $("#edit").click (function() {
-    let ime = document.getElementById('ime-uredi').value;
-    let priimek = document.getElementById('priimek-uredi').value;
-    let email = document.getElementById('email-uredi').value;
-    let geslo = document.getElementById('geslo-uredi').value;
-    let naziv_podjetja = document.getElementById('naziv_podjetja-uredi').value;
-    let davcna = document.getElementById('davcna-uredi').value;
-    let zacetek_delovanja = document.getElementById('zacetek_delovanja-uredi').value;
-    let uspesnost_poslovanja = document.getElementById('uspesnot_poslovanja-uredi').value;
+    let ime = document.getElementById('i_ime_uredi').value;
+    let priimek = document.getElementById('i_priimek_uredi').value;
+    let email = document.getElementById('i_email_uredi').value;
+    let geslo = document.getElementById('i_geslo_uredi').value;
+    let naziv_podjetja = document.getElementById('naziv_podjetja_uredi').value;
+    let davcna = document.getElementById('davcna').value;
+    let zacetek_delovanja = document.getElementById('zacetek_delovanja_uredi').value;
+    let uspesnost_poslovanja = document.getElementById('uspesnost_poslovanja_uredi').value;
     
     let podatkiUporabnika = {
         'ime': ime,
@@ -42,34 +42,40 @@ $.ajax({
     .done(function (data) {
         if (data) {
             for (let i = 0; i < data.length; i++) {
-                
-                    let uporabnik = data[i];
-                    if ($("#i_ime_uredi").val(uporabnik.ime)) {
-                        $("#i_ime_uredi").focus();
-                    }
-                    if ($("#i_priimek_uredi").val(uporabnik.priimek)) {
-                        $("#i_priimek_uredi").focus();
-                    }
-                    /*if ($("#i_postna_stevilka_uredi").val(uporabnik.po)) {
-                        $("#i_postna_stevilka_uredi").focus();
-                    }
-                    if ($("#i_kraj_uredi").val(uporabnik.geslo)) {
-                        $("#i_kraj_uredi").focus();
-                    }*/
-                    if ($("#i_ulica_uredi").val(uporabnik.ulica)) {
-                        $("#i_ulica_uredi").focus();
-                    }
-                    if ($("#i_hisna_stevilka_uredi").val(uporabnik.naziv_podjetja)) {
-                        $("#i_hisna_stevilka_uredi").focus();
-                    }
-                    if ($("#i_hisna_stevilka_uredi").val(uporabnik.naziv_podjetja)) {
-                        $("#i_hisna_stevilka_uredi").focus();
-                    }
-                    if ($("#i_email_uredi").val(uporabnik.email)) {
-                        $("#i_email_uredi").focus();
-                    }
-                    
-                    break;
+                let uporabnik = data[i];
+
+                if ($("#i_priimek_uredi").val(uporabnik.priimek)) {
+                    $("#i_priimek_uredi").focus();
+                }
+                /*if ($("#i_postna_stevilka_uredi").val(uporabnik.po)) {
+                    $("#i_postna_stevilka_uredi").focus();
+                }
+                if ($("#i_kraj_uredi").val(uporabnik.geslo)) {
+                    $("#i_kraj_uredi").focus();
+                }
+                if ($("#i_ulica_uredi").val(uporabnik.ulica)) {
+                    $("#i_ulica_uredi").focus();
+                }
+                if ($("#i_hisna_stevilka_uredi").val(uporabnik.naziv_podjetja)) {
+                    $("#i_hisna_stevilka_uredi").focus();
+                }
+                */
+                if ($("#davcna").val(uporabnik.davcna)) {
+                    $("#davcna").focus();
+                }
+                if ($("#naziv_podjetja_uredi").val(uporabnik.naziv_podjetja)) {
+                    $("#naziv_podjetja_uredi").focus();
+                }
+                if ($("#uspesnost_poslovanja_uredi").val(uporabnik.uspesnost_poslovanja)) {
+                    $("#uspesnost_poslovanja_uredi").focus();
+                }
+                if ($("#i_email_uredi").val(uporabnik.email)) {
+                    $("#i_email_uredi").focus();
+                }
+                if ($("#i_ime_uredi").val(uporabnik.ime)) {
+                    $("#i_ime_uredi").focus();
+                }
+                break;
             }        
         }
     });
